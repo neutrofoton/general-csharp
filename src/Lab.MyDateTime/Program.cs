7d1aa6b72dea816111e1fcfd69664ed7d99f96aa
neutro <neutro@mymail.com>
2024-07-28 21:41:31 +0700

    add timezone sample

---
+++ src/Lab.MyDateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿var timezones = TimeZoneInfo.GetSystemTimeZones();
+
+// foreach(var tz in timezones)
+// {
+//     Console.WriteLine($"{tz.Id}");
+// }
+
+var tzJakarta = timezones.FirstOrDefault(t => t.Id.ToLower().Contains("jakarta"));
+
+Console.WriteLine($"{tzJakarta!.Id}");
+
+string timeInputUtc = "2024-07-27T17:00:00.000Z";
+
+DateTime dateTimeInputUtc;
+bool isValidTime = DateTime.TryParse(timeInputUtc,out dateTimeInputUtc);
+
+if(isValidTime)
+{
+    DateTime outDateTimeJakarta = TimeZoneInfo.ConvertTime(dateTimeInputUtc,tzJakarta);
+    Console.WriteLine($"{outDateTimeJakarta.ToString("dd-MMM-yyyy hh:mm:ss")}");
+}
